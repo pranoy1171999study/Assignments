971804055b20405fe91186ffe48fc1c26719a93d
Pranoy Patra <96573528+pranoy1171999study@users.noreply.github.com>
2023-01-17 21:25:19 +0530

    Create Pig.java

---
+++ PlayPig/PlayPigMultiplayer/Pig.java
@@ -0,0 +1,31 @@
+package assignments.PlayPig.PlayPigMultiplayer;
+
+public class Pig {
+    private int totalScore;
+    private int turns;
+    private String name;
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public void setTurns(int turns) {
+        this.turns = turns;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
