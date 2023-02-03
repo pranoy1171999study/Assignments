629a6a0f349b167081b5a3957fb12ec0739760c7
Pranoy Patra <96573528+pranoy1171999study@users.noreply.github.com>
2023-02-03 21:29:33 +0530

    Create Pig.java

---
+++ PigDice/Pig.java
@@ -0,0 +1,22 @@
+package com.monocept.test;
+
+public class Pig {
+    private int totalScore;
+    private int turns;
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public void setTurns(int turns) {
+        this.turns = turns;
+    }
+}
