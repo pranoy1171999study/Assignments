8bb7f66d4e2514f697d594e0cc5c0934d913f61d
Pranoy Patra <96573528+pranoy1171999study@users.noreply.github.com>
2023-02-03 21:29:59 +0530

    Create PlayPig.java

---
+++ PigDice/PlayPig.java
@@ -0,0 +1,61 @@
+package com.monocept.test;
+
+import java.util.Scanner;
+
+public class PlayPig {
+	int turn = 0;
+    Pig pig = new Pig();
+    
+    public int rollDice() {
+        int die = 1 + (int) (Math.random() * 5);
+        System.out.println("Die: " + die);
+        return die;
+    }
+
+    private void welcome() {
+        System.out.print(
+                "Let's Play PIG!\n* See how many turns it takes you to get to 20.\n* Turn ends when you hold or roll a 1.\n* If you roll a 1, you lose all points for the turn.\n* If you hold, you save all points for the turn.\n\n");
+    }
+    private void rollHold() {
+    	int tempScore = 0;
+    	do {
+            System.out.println("Roll or hold? (r/h): ");
+            Scanner sc = new Scanner(System.in);
+            char c = sc.next().charAt(0);
+            if (c == 'r') {
+                int tempRoll = rollDice();
+                if (tempRoll == 1) {
+                    System.out.println("Turn Over. No Score");
+                    break;
+                }
+                tempScore += tempRoll;
+            } else if (c == 'h') {
+                System.out.println("Score for turn: " + tempScore);
+                pig.setTotalScore(pig.getTotalScore() + tempScore);
+                pig.setTurns(turn);
+                System.out.println("total score: " + pig.getTotalScore());
+                break;
+            } else {
+                System.out.println("Wrong input!(enter r/h)");
+            }
+        } while (true);
+    }
+    public void start() {
+    	turn=0;
+        welcome();
+        do {
+            turn++;
+            System.out.println("TURN " + turn);
+            rollHold();
+
+        } while (pig.getTotalScore() < 20);
+        if (pig.getTotalScore() >= 20) {
+            System.out.print("You finished in " + pig.getTurns() + " turns!\nGame over!");
+        }
+    }
+
+    public static void main(String[] args) {
+        PlayPig game = new PlayPig();
+        game.start();
+    }
+}
