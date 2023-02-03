7f8bea5eec8d42d8947bc3f01eb212e9af98d60b
Pranoy Patra <96573528+pranoy1171999study@users.noreply.github.com>
2023-02-03 19:43:40 +0530

    Create Player.java

---
+++ Tic-Tac-Tao/Player.java
@@ -0,0 +1,17 @@
+package com.monocept.TicTacToe;
+
+public class Player {
+	private String name;
+	public Player() {
+		name="";
+	}
+	public Player(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
