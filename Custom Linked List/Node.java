23dbb3bde6ae16d751b3eacf7945f33673914752
Pranoy Patra <96573528+pranoy1171999study@users.noreply.github.com>
2023-02-15 22:06:47 +0530

    Update Node.java

--- Custom Linked List/Node.java
+++ Custom Linked List/Node.java
@@ -3,8 +3,8 @@ package com.monocept.model;
 import java.util.Iterator;
 
 public class Node<E> {
-	E value;
-	Node<E> next=null;
+	public E value;
+	public Node<E> next=null;
 	
 	public Node(E value) {
 		this.value=value;
